a163a5e72bf744665c1fcf72378721cad6ffc448
PavelK6896 <79208776896@yandex.ru>
2021-12-24 21:54:27 +0300

    update regex 1

--- regex1/src/app/web/pavelk/regex1/Main1.java
+++ regex1/src/app/web/pavelk/regex1/Main1.java
@@ -18,3 +18,21 @@ public class Main1 {
         System.out.println("234234234242.234234.23432".replaceAll(regex2, ""));
     }
 }
+
+//Содержит ли строка хоть одну букву.
+//^ - начало строки
+//$ - конец строки
+//. - любой символ
+//X* - ноль или более раз
+//X+ - один или несколько раз
+//(?=X) - с помощью положительного обзора нулевой ширины
+//\p{L} - любая буква Юникода
+class Main2 {
+    public static void main(String[] args) {
+        System.out.println("...1".matches("^(?=.*[a-zA-Z]).+$"));
+        System.out.println("..w.1".matches("^(?=.*[a-zA-Z]).+$"));
+        System.out.println("...12@.1".matches("^(?=.*[\\p{L}]).+$"));
+        System.out.println(".ф..1".matches("^(?=.*[\\p{L}]).+$"));
+        System.out.println(".@./@#2..1н".matches("^(?=.*[\\p{L}]).+$"));
+    }
+}
